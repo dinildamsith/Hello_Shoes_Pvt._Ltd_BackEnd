1f2a252cd7cad7c52cff4c43e64a350f16dffb9a
dinildamsith <dinildamsith52@gmail.com>
2024-04-22 12:24:08 +0530

    supplier data convert added

--- src/main/java/lk/ijse/hello_shoes_shop_backend/convert/DataConvert.java
+++ src/main/java/lk/ijse/hello_shoes_shop_backend/convert/DataConvert.java
@@ -1,8 +1,10 @@
 package lk.ijse.hello_shoes_shop_backend.convert;
 
 import lk.ijse.hello_shoes_shop_backend.Dto.CustomerDto;
+import lk.ijse.hello_shoes_shop_backend.Dto.SupplierDto;
 import lk.ijse.hello_shoes_shop_backend.Dto.UserDto;
 import lk.ijse.hello_shoes_shop_backend.entity.CustomerEntity;
+import lk.ijse.hello_shoes_shop_backend.entity.SupplierEntity;
 import lk.ijse.hello_shoes_shop_backend.entity.UserEntity;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -13,19 +15,28 @@ import org.springframework.stereotype.Component;
 public class DataConvert {
     private final ModelMapper modelMapper;
 
+
+    // User Data
     public UserDto userEntityConvertUserDto(UserEntity userEntity){
         return modelMapper.map(userEntity, UserDto.class);
     }
-
     public UserEntity userDtoConvertUserEntity(UserDto userDto){
         return modelMapper.map(userDto, UserEntity.class);
     }
 
+    //Customer Data
     public CustomerDto customerEntityConvertCustomerDto(CustomerEntity customerEntity){
         return modelMapper.map(customerEntity, CustomerDto.class);
     }
-
     public CustomerEntity customerDtoConvertCustomerEntity(CustomerDto customerDto){
         return modelMapper.map(customerDto, CustomerEntity.class);
+    }
+
+    //Supplier Data
+    public SupplierEntity supplierDtoConvertSupplierEntity(SupplierDto supplierDto){
+        return modelMapper.map(supplierDto,SupplierEntity.class);
+    }
+    public SupplierDto supplierEntityConvertSupplierDto(SupplierEntity supplierEntity){
+        return modelMapper.map(supplierEntity, SupplierDto.class);
     }
 }

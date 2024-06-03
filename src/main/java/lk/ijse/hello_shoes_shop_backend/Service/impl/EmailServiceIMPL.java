9fd3d3ae334f90e817653543f0ce0d4b47b5d4ec
dinildamsith <dinildamsith52@gmail.com>
2024-06-03 10:39:52 +0530

    log added email service

--- src/main/java/lk/ijse/hello_shoes_shop_backend/Service/impl/EmailServiceIMPL.java
+++ src/main/java/lk/ijse/hello_shoes_shop_backend/Service/impl/EmailServiceIMPL.java
@@ -4,6 +4,8 @@ import lk.ijse.hello_shoes_shop_backend.Dao.CustomerRepo;
 import lk.ijse.hello_shoes_shop_backend.Dto.MailDto;
 import lk.ijse.hello_shoes_shop_backend.Service.EmailService;
 import lk.ijse.hello_shoes_shop_backend.entity.CustomerEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,19 +19,24 @@ public class EmailServiceIMPL implements EmailService {
 
     @Autowired
     CustomerRepo customerRepo;
+    Logger logger = LoggerFactory.getLogger(EmailServiceIMPL.class);
 
     @Override
     public void birthdayGreetingsToCustomers() {
 
         try {
             List<CustomerEntity> all = customerRepo.findAll();
-            Date date = new Date();
-            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-            String formattedCurrentDate = sdf.format(date);
+            Date currentDate = new Date();
+//            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//            String formattedCurrentDate = sdf.format(date);
 
             for (CustomerEntity customer : all) {
 
-                if (formattedCurrentDate.equals(String.valueOf(customer.getBirthDay()))) {
+
+                int CustBodMonth = customer.getBirthDay().getMonth()+1;
+                int CustBodDay = customer.getBirthDay().getDate();
+
+                if (currentDate.getMonth()+1 == CustBodMonth && currentDate.getDate() == CustBodDay) {
                     MailDto mailDto = new MailDto();
                     mailDto.setTo(customer.getEmail()); // Receiver's email
                     mailDto.setSubject("\uD83C\uDF89 Happy Birthday from Hello Shoe Shop! \uD83C\uDF82"); // Email subject
@@ -38,7 +45,7 @@ public class EmailServiceIMPL implements EmailService {
                     thread.start();
 
                 }else{
-                    System.out.println("Pukkkk...");
+                    logger.info("Today No Birthdays");
                 }
 
 

49c1b3dc138b2d2838efc0944d5bf7855c6367c5
dinildamsith <dinildamsith52@gmail.com>
2024-06-03 10:40:33 +0530

    log added stock service

--- src/main/java/lk/ijse/hello_shoes_shop_backend/Service/impl/StockServiceIMPL.java
+++ src/main/java/lk/ijse/hello_shoes_shop_backend/Service/impl/StockServiceIMPL.java
@@ -4,6 +4,8 @@ import jakarta.transaction.Transactional;
 import lk.ijse.hello_shoes_shop_backend.Dao.SizeRepo;
 import lk.ijse.hello_shoes_shop_backend.Service.StockService;
 import lk.ijse.hello_shoes_shop_backend.entity.StockEntity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +17,8 @@ import java.util.List;
 public class StockServiceIMPL implements StockService {
     @Autowired
     SizeRepo sizeRepo;
+
+    Logger logger = LoggerFactory.getLogger(StockServiceIMPL.class);
     @Override
     public List<StockEntity> getAllStock() {
         List<StockEntity> allStock = sizeRepo.findAll();
@@ -30,8 +34,9 @@ public class StockServiceIMPL implements StockService {
         for (StockEntity stockEntity:allItemsStock) {
             String stockId = stockEntity.getStockId();
             if (50<Integer.parseInt(stockEntity.getQty())){
-                System.out.println("Have");
+                logger.info(stockEntity.getStockId() +" Availed Item");
             }else{
+                logger.info(stockId+" This Item Low Quantity");
                 lowItemMassages.add(stockId + " " + "Item Low Quantity Have Stock.Please Buy This Item");
                 //return stockId + " " + "Item Low Quantity Have Stock.Please Buy This Item";
             }
